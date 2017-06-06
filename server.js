cbc17516b2fc656a3d6212f485ce0c9dbe0c5718
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-06 14:14:46 +0700

    Fun with GraphQL api, instead of using schema languages

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var graphql = require('graphql');
 
-var schema = buildSchema(`
-  type Query {
-    ip: String
-  }
-`);
-
-function loggingMiddleware(req, res, next) {
-  console.log('ip:', req.ip);
-  next();
-}
-
-var root = {
-  ip: function (args, request) {
-    return request.ip;
-  }
+// Maps id to User object
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  },
 };
 
+// Define the User type
+var userType = new graphql.GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+  }
+});
+
+// Define the Query type
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: graphql.GraphQLString }
+      },
+      resolve: function (_, {id}) {
+      	console.log(_);
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
+
 var app = express();
-app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
